fd2606406d4e0f64b51f76146bb5fa5440e7a6d5
alejandro1osorio <alejowosorio2986@gmail.com>
2024-10-02 13:51:21 -0500

    shadow

--- src/components/SectionOne.vue
+++ src/components/SectionOne.vue
@@ -4,7 +4,21 @@
   
       <!-- Imagen Derecha -->
       <div class="right-image-container">
-        <img src="../assets/section-one/Portada-ciclo-6.1.png" alt="Imagen derecha" class="responsive-image"/>
+        <img src="../assets/section-one/Portada-ciclo-6.1.png" alt="Imagen derecha" class="responsive-image" />
+      </div>
+  
+      <!-- Imagen Inferior Izquierda -->
+      <img src="../assets/section-one/Nube-Portada-ciclo-6.png" alt="Imagen inferior izquierda" class="bimg-responsive" />
+  
+      <!-- Texto en la esquina inferior izquierda -->
+      <div class="text-container">
+        <p class="responsive-text">
+          En sus manos<br />
+          está la posibilidad<br />
+          de transformar sonrisas<br />
+           y mejorar la calidad de vida<br />
+          de muchas personas.
+        </p>
       </div>
     </section>
   </template>
@@ -20,7 +34,7 @@
     position: relative;
     width: 100%;
     height: 100vh;
-    overflow: hidden; /* Para evitar desbordamiento */
+    overflow: hidden; 
   }
   
   .background-container {
@@ -40,7 +54,7 @@
     top: 0;
     right: 0;
     height: 100%; 
-    width: 50%; 
+    width: 30%; 
     display: flex;
     justify-content: center;
     align-items: center;
@@ -51,6 +65,43 @@
     width: 100%;
     height: 100%; 
     object-fit: cover; 
+  }
+  
+  .bimg-responsive {
+    position: absolute;
+    bottom: 0; 
+    left: 0; 
+    width: 100%; 
+    height: 95%; 
+    object-fit: cover; 
+    z-index: 1; 
+  }
+  
+  .text-container {
+    position: absolute;
+    bottom: 50px; 
+    left: 116px; 
+    z-index: 2; 
+    color: #B28236; 
+  }
+  
+  .responsive-text {
+    font-size: 2.9rem; 
+    text-align: left; 
+    margin: 0; 
+  }
+  
+  /* Estilos responsivos */
+  @media (max-width: 768px) {
+    .responsive-text {
+      font-size: 1.2rem; 
+    }
+  }
+  
+  @media (max-width: 480px) {
+    .responsive-text {
+      font-size: 1rem; 
+    }
   }
   
   @media (min-width: 768px) and (max-width: 1024px), /* Tablets en modo retrato */
