76af4f79c67380e14c41e9c3a3d391457bda35f4
alejandro1osorio <alejowosorio2986@gmail.com>
2024-10-04 15:06:30 -0500

    letras centro

--- src/components/SectionThree.vue
+++ src/components/SectionThree.vue
@@ -9,22 +9,34 @@
 
     <div class="imagenes-abajo">
       <!-- Contenedor de las imágenes 1, 2, y 3 -->
-      <div class="contenedor-izquierdo" style="margin-left: 40px;">
-        <img src="../assets/section-three/círculo.webp" alt="Imagen 1" />
+      <div class="contenedor-izquierdo" style="margin-left: 60px;">
+        <div class="imagen-superior" style="position: relative;">
+          <img src="../assets/section-three/círculo.webp" alt="Imagen 1" />
+          <img
+            src="../assets/section-three/Estaño.webp"
+            alt="Imagen Superior 1"
+            class="imagen-superior-1"
+          />
+        </div>
         <img src="../assets/section-three/+.webp" alt="Imagen 2" />
-        <img src="../assets/section-three/círculo.webp" alt="Imagen 3" />
+        <div class="imagen-superior" style="position: relative;">
+          <img src="../assets/section-three/círculo.webp" alt="Imagen 3" />
+          <img
+            src="../assets/section-three/Estaño.webp"
+            alt="Imagen Superior 2"
+            class="imagen-superior-2"
+          />
+        </div>
       </div>
 
       <!-- Contenedor de las imágenes 4 y 5 -->
-      <div class="contenedor-derecho" style="margin-right: 40px;">
+      <div class="contenedor-derecho" style="margin-right: 60px;">
         <img
           src="../assets/section-three/Fómula Bio Activo.webp"
           alt="Imagen 4"
+          style="margin-right: 19px;"
         />
-        <img
-          src="../assets/section-three/Fórmula No Bio Activo.webp"
-          alt="Imagen 5"
-        />
+        <img src="../assets/section-three/Fórmula No Bio Activo.webp" alt="Imagen 5" />
       </div>
     </div>
   </section>
@@ -72,6 +84,24 @@ section {
 .contenedor-izquierdo img,
 .contenedor-derecho img {
   margin-left: 10px;
+}
+
+/* Nueva clase para el contenedor de las imágenes superpuestas */
+.imagen-superior {
+  position: relative;
+  display: inline-block;
+}
+
+/* Clases para las imágenes que se superponen encima de las imágenes 1 y 3 */
+.imagen-superior-1,
+.imagen-superior-2 {
+  position: absolute;
+  top: 50%; /* Centrar verticalmente */
+  left: 50%; /* Centrar horizontalmente */
+  transform: translate(-50%, -50%); /* Ajusta para centrar completamente */
+  width: 170px; /* Ajusta el tamaño si es necesario */
+  height: auto;
+  z-index: 1; /* Para asegurarse de que esté encima de las imágenes base */
 }
 
 /* Estilos responsivos para tablet */
