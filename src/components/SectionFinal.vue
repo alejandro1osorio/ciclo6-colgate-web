f985ff7d915aafdae242317016f6fb18f921a7f5
alejandro1osorio <alejowosorio2986@gmail.com>
2024-10-08 09:19:47 -0500

    video

--- src/components/SectionFinal.vue
+++ src/components/SectionFinal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="iframe-section">
     <iframe
-      src="https://player.vimeo.com/video/999685698?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+      src="https://player.vimeo.com/video/1017554102?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
       class="iframe-content"
       frameborder="0"
       allowfullscreen
